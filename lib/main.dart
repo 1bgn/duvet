0b8a964f85eb29076ad88a38e7a7d0ae98b40823
Semion <trapce@mail.ru>
2023-11-13 07:08:03 +0800

    STABLE10

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ import 'package:projects/domain/model/book_data.dart';
 import 'package:projects/domain/text_decorator/text_decorator.dart';
 import 'package:projects/presentation/component/hyphenated_text_component.dart';
 import 'package:projects/presentation/component/pager.dart';
+import 'package:projects/presentation/screens/select_book/select_book.dart';
 
 import 'domain/model/styled_element.dart';
 
@@ -28,66 +29,9 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home:  SelectBook(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
 
-
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  bool bookIsLoaded  = false;
-  late final List<StyledElement> _elements;
-  late final int wordsInBook;
-
-  @override
-  void initState() {
-    rootBundle.load("assets/books/book5.fb2").then((value) {
-      _elements = HyphenatedTextFactory.elementsFromXml(xmlText: utf8.decode(value.buffer.asUint8List()));
-      // _elements.take(25).forEach((element) {
-      //   print("FIRST TEXT $element");
-      // });
-      int index = 0;
-      for (var element in _elements) {
-        element.index = index;
-        index += element.text.length;
-      }
-
-      wordsInBook =  (_elements.map((e) => e.text.split(" ").length).fold(0, (previousValue, element) => previousValue+element));
-      setState(() {
-        bookIsLoaded = true;
-
-      });
-    });
-    super.initState();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(child: StatefulBuilder(builder: (BuildContext context, void Function(void Function()) setState) {
-        return LayoutBuilder(
-            builder: (context,size) {
-
-              if(bookIsLoaded){
-                return Row(children: [
-                  Expanded(child: Pager(bookData: BookData(countWordsInBook: wordsInBook,elements: _elements,size: size,),))
-                ],);
-              }else{
-return CircularProgressIndicator();
-              }
-            }
-        );
-      },)),
-    );
-  }
-}
---
+++ lib/presentation/screens/select_book/select_book.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:projects/presentation/screens/book_page/book_page.dart';
+
+class SelectBook extends StatefulWidget{
+  @override
+  State<SelectBook> createState() => _SelectBookState();
+}
+
+class _SelectBookState extends State<SelectBook> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: SafeArea(child: Center(child: MaterialButton(onPressed: () {
+      Navigator.push(context, MaterialPageRoute(builder: (context)=>BookPage()));
+    },child: Text("Open book"),),)),);
+  }
+}
